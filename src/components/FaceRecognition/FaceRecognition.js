c8d39a6e1a247064784f2fbaef04d037bbdb0f59
SivaramPg <sivaramp95@gmail.com>
2018-07-06 17:34:23 +0530

    changing alternate pic below the link element

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -8,7 +8,7 @@ const FaceRecognition = ({ imageUrl, box }) => {
         <img
           id="inputImage"
           src={imageUrl}
-          alt="Sample Img"
+          alt=" "
           width="500px"
           height="auto"
         />
