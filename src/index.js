9161687e3f8ef4d3d1181f5c05a5e86b4bff978b
Sivaram Pandariganthan <sivarampg95@gmail.com>
2020-05-30 00:39:04 +0530

    remove service worker

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './container/App';
-import registerServiceWorker from './registerServiceWorker';
+
 import 'tachyons';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
